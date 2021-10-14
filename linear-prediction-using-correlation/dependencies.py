63fb98a4483425174d8cef23147885a0cd9a139d
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2021-10-14 10:31:28 +0700

    linear prediction model and using kwarg argument model

--- linear-prediction-using-correlation/dependencies.py
+++ linear-prediction-using-correlation/dependencies.py
@@ -1,4 +1,4 @@
-def auto_correlation(type, data):
+def auto_correlation(type, data, **kwargs):
     """Doing correlation to the signal itself using designed mathematics equation. Time shifted to the length of data.
 
     Parameters
@@ -7,6 +7,8 @@ def auto_correlation(type, data):
         Function type: normal for using the direct correlation, inversefourier using magnitude spectrum
     data : 1D array
         Input of the function, data signal
+    **kwargs : Dict
+        Add "timelag" length for using "timelag" type, ex : "timelag" = 5
 
     Returns
     -------
@@ -16,11 +18,12 @@ def auto_correlation(type, data):
     
     from numpy import array, zeros, correlate, exp, pi
     
-    result = array(zeros(len(data)), dtype=complex)
-    magnitude_spectrum = array(zeros(len(data)), dtype=complex)
     if type == "normal":
+        result = array(zeros(len(data)), dtype=complex)
         return [x/len(data) for x in correlate(data, data, mode="same")]
     elif type == "inversefourier":
+        result = array(zeros(len(data)), dtype=complex)
+        magnitude_spectrum = array(zeros(len(data)), dtype=complex)
         for i in range(len(data)):
             for j in range(len(data)):
                 magnitude_spectrum[i] += (data[j]**2)*exp(-2j*pi*i*j/len(data))
@@ -29,6 +32,17 @@ def auto_correlation(type, data):
             for j in range(len(data)):
                 result[i] += (magnitude_spectrum[j]**2)*exp(2j*pi*i*j/len(magnitude_spectrum))
             result[i] = result[i]/len(magnitude_spectrum)
+        return result
+    elif type == "timelag":
+        if not "timelag" in kwargs:
+            raise Exception("Please check the available type!") 
+        timelag = kwargs["timelag"]
+        result = array(zeros(timelag), dtype=complex)
+        for i in range(timelag):
+            for j in range(len(data)):
+                if j < i:
+                    continue
+                result[i] += data[j]*data[j-i]
         return result
     else: raise Exception("Please check the available type!") 
     
